f2a9794d8ba7ed90dde6ab3b4dfcd4ac76a956e3
ChillibyteUK <dan@chillibyte.co.uk>
2025-07-30 17:30:47 +0100

    index

--- index.php
+++ index.php
@@ -32,59 +32,40 @@ get_header();
 			</div>
 		</div>
 	</section>
-    <section class="latest_insights">
+    <section class="latest_posts mt-5">
         <div class="container pb-5">
             <div class="row g-4 w-100">
             <?php
             // Custom query to include both published and scheduled posts
             $args = array(
                 'post_type'      => 'post',
-                'post_status'    => array('publish', 'future'), // Include published and scheduled posts
+                'post_status'    => array( 'publish', 'future' ), // Include published and scheduled posts.
                 'orderby'        => 'date',
-                'order'          => 'ASC', // Ascending order
-                'posts_per_page' => -1,    // Get all posts
+                'order'          => 'ASC', // Ascending order.
+                'posts_per_page' => -1,    // Get all posts.
             );
 
-            $query = new WP_Query($args);
+            $q = new WP_Query( $args );
 
-            if ($query->have_posts()) {
-                while ($query->have_posts()) {
-                    $query->the_post();
-                    $img = get_the_post_thumbnail_url(get_the_ID(), 'large');
-                    if (!$img) {
-                        $img = get_stylesheet_directory_uri() . '/img/default-blog.jpg';
-                    }
-                    ?>
-                    <div class="col-md-4">
-						<?php
-					if (get_post_status() === 'publish') {
-                            ?>
-                        <div class="podcast-card">
-                            <a href="<?php the_permalink(); ?>" class="podcast-card--published">
-                                <div class="podcast-card__date"><?= get_the_date(); ?></div>
-                                <h3 class="podcast-card__title"><?= the_title(); ?></h3>
-                                <div class="podcast-card__excerpt"><?= get_the_excerpt(); ?></div>
-                                <div class="podcast-card__status">LIVE NOW</div>
-                                <?= get_the_post_thumbnail( get_the_ID(), 'large', array( 'class' => 'podcast-card__thumbnail' ) ); ?>
-                            </a>
-                        </div>
-                            <?php
-                        } else {
-                            ?>
-                        <div class="podcast-card">
-                            <div class="podcast-card--scheduled">
-                                <div class="podcast-card__date"><?= get_the_date(); ?></div>
-                                <h3 class="podcast-card__title"><?= the_title(); ?></h3>
-                                <div class="podcast-card__excerpt"><?= get_the_excerpt(); ?></div>
-                                <div class="podcast-card__status">COMING SOON</div>
-                                <?= get_the_post_thumbnail( get_the_ID(), 'large', array( 'class' => 'podcast-card__thumbnail' ) ); ?>
-                            </div>
-                        </div>
-                            <?php
-                        }
-						?>
-                    </div>
-                    <?php
+            if ( $q->have_posts() ) {
+				while ( $q->have_posts() ) {
+					$q->the_post();
+					?>
+					<div class="col-md-6 col-lg-4" data-aos="fade" data-aos-delay="<?= esc_attr( $d ); ?>">
+						<a class="latest_posts__item" href="<?= esc_url( get_permalink() ); ?>">
+							<div class="latest_posts__image">
+								<?= get_the_post_thumbnail( get_the_ID(), 'medium', array( 'class' => 'img-fluid' ) ); ?>
+							</div>
+							<div class="post_meta">
+								<span><i class="fa-regular fa-calendar"></i> <?= esc_html( get_the_date( 'jS F Y' ) ); ?></span>
+								<span><i class="fa-regular fa-clock"></i> <?= esc_html( estimate_reading_time_in_minutes( get_the_content() ) ); ?> minute read</span>
+
+							</div>
+							<h3 class="latest_posts__title"><?= esc_html( get_the_title() ); ?></h3>
+						</a>
+					</div>
+					<?php
+					$d += 100;
                 }
             } else {
                 echo '<p>No posts found.</p>';
