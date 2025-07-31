942ef37f242d931a52b525b765a8aa79a85b07bd
ChillibyteUK <dan@chillibyte.co.uk>
2025-07-31 12:41:26 +0100

    contact

--- page-templates/blocks/cb-contact.php
+++ page-templates/blocks/cb-contact.php
@@ -14,8 +14,8 @@ defined( 'ABSPATH' ) || exit;
 			<div class="col-md-6">
 				<p>For any questions or to discuss your property needs, please call, email, or fill in the form.</p>
 				<ul class="fa-ul">
-					<li><span class="fa-li"><i class="fa-solid fa-phone"></i></span> <a href="tel:<?= esc_attr( get_field( 'contact_phone', 'option' ) ); ?>"><?= esc_html( get_field( 'contact_phone', 'option' ) ); ?></a></li>
-					<li><span class="fa-li"><i class="fa-solid fa-envelope"></i></span> <a href="mailto:<?= esc_attr( antispambot( get_field( 'contact_email', 'option' ) ) ); ?>"><?= esc_html( antispambot( get_field( 'contact_email', 'option' ) ) ); ?></a></li>
+					<li><span class="fa-li"><i class="fa-solid fa-phone has-accent-500-color"></i></span> <a href="tel:<?= esc_attr( get_field( 'contact_phone', 'option' ) ); ?>"><?= esc_html( get_field( 'contact_phone', 'option' ) ); ?></a></li>
+					<li><span class="fa-li"><i class="fa-solid fa-envelope has-accent-500-color"></i></span> <a href="mailto:<?= esc_attr( antispambot( get_field( 'contact_email', 'option' ) ) ); ?>"><?= esc_html( antispambot( get_field( 'contact_email', 'option' ) ) ); ?></a></li>
 				</ul>
 			</div>
 			<div class="col-md-6">
