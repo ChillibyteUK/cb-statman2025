671c17a35de6f2bda6557ea01e0c0d1e6ffc94cd
ChillibyteUK <dan@chillibyte.co.uk>
2025-07-31 12:34:06 +0100

    contact

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -19,6 +19,23 @@ function acf_blocks() {
 
         acf_register_block_type(
             array(
+                'name'            => 'cb_contact',
+                'title'           => __( 'CB Contact' ),
+                'category'        => 'layout',
+                'icon'            => 'cover-image',
+                'render_template' => 'page-templates/blocks/cb-contact.php',
+                'mode'            => 'edit',
+                'supports'        => array(
+                    'mode'      => false,
+                    'anchor'    => true,
+                    'className' => true,
+                    'align'     => true,
+                ),
+            )
+        );
+
+        acf_register_block_type(
+            array(
                 'name'            => 'cb_contact_cta',
                 'title'           => __( 'CB Contact CTA' ),
                 'category'        => 'layout',
