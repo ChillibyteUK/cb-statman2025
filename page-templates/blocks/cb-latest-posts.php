71c705ed272ad53a9bb3065016b221d809448d3d
ChillibyteUK <dan@chillibyte.co.uk>
2025-08-01 15:05:02 +0100

    index filtering

--- page-templates/blocks/cb-latest-posts.php
+++ page-templates/blocks/cb-latest-posts.php
@@ -10,7 +10,7 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <section class="latest_posts has-secondary-300-background-color py-5">
 	<div class="container">
-		<h2 class="fancy" data-aos="fade">News &amp; Advice</h2>
+		<h2 class="fancy" data-aos="fade">News &amp; Insights</h2>
 		<?php
 		$q = new WP_Query(
 			array(
@@ -27,10 +27,28 @@ defined( 'ABSPATH' ) || exit;
 				$d = 0;
 				while ( $q->have_posts() ) {
 					$q->the_post();
+					// get first category.
+					$first_category = get_the_category();
+					if ( ! empty( $first_category ) && ! is_wp_error( $first_category ) ) {
+						$first_category = $first_category[0];
+					} else {
+						$first_category = null;
+					}
 					?>
 					<div class="col-md-6 col-lg-4" data-aos="fade" data-aos-delay="<?= esc_attr( $d ); ?>">
 						<a class="latest_posts__item" href="<?= esc_url( get_permalink() ); ?>">
 							<div class="latest_posts__image">
+								<?php
+								if ( $first_category ) {
+									?>
+									<span class="badge"><?= esc_html( $first_category->name ); ?></span>
+									<?php
+								} else {
+									?>
+									<span class="badge">News</span>
+									<?php
+								}
+								?>
 								<?= get_the_post_thumbnail( get_the_ID(), 'medium', array( 'class' => 'img-fluid' ) ); ?>
 							</div>
 							<div class="post_meta ps-3">
@@ -50,7 +68,7 @@ defined( 'ABSPATH' ) || exit;
 		wp_reset_postdata();
 		?>
 		<div class="text-center">
-			<a class="button button-solid" href="<?= esc_url( get_permalink( get_option( 'page_for_posts' ) ) ); ?>">All News &amp; Advice</a>
+			<a class="button button-solid" href="<?= esc_url( get_permalink( get_option( 'page_for_posts' ) ) ); ?>">All News &amp; Insights</a>
 		</div>
 	</div>
 </section>
